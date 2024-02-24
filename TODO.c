6fb64ef87c5647ce2f1a235e44089d8994e5145b
j@gmail.com <j@gmail.com>
2024-02-24 14:41:55 +0100

    removed example files --- added first version of TODO for the project

---
+++ TODO.c
@@ -0,0 +1,19 @@
+--- Makefile
+
+--- Open - Read - Close
+
+--- Get dictionnary into memory
+	-- parse dic
+		-- get rid of spaces
+		-- get length of it
+	-- choose data structure
+		-- malloc it <-- length
+		-- put stuff into it
+		-- free it !
+
+--- How to parse int to write ?
+	-- go into map and choose correct entries
+
+--- Main.c
+	-- if argc == 1, if argc == 2
+	-- exceptions & error handling
