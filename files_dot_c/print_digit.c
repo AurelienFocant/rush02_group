1d383d6b2941c4cb0e3a39d7ce70983af225b428
Salim Benbella <sbenbell@fu-r6-p5.s19.be>
2024-02-25 19:31:17 +0100

    print_digit .c .h added and all files norminated

---
+++ files_dot_c/print_digit.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_digit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenbell <sbenbell@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 19:14:09 by sbenbell          #+#    #+#             */
+/*   Updated: 2024/02/25 19:28:35 by sbenbell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "print_digit.h"
+
+void	ft_print_zero(char c, char **keys, char **values)
+{
+	char	str[2];
+	int		index;
+
+	str[0] = c;
+	str[1] = '\0';
+	index = ft_find_index(str, keys);
+	ft_putstr(values[index]);
+	ft_putstr("\n");
+}
+
+void	ft_print_hundreds(char c, char **keys, char **values)
+{
+	int		index;
+	char	str[2];
+
+	str[0] = c;
+	str[1] = '\0';
+	if (c != 0)
+	{
+		index = ft_find_index(str, keys);
+		ft_putstr(values[index]);
+		index = ft_find_index("100", keys);
+		ft_putstr(" ");
+		ft_putstr(values[index]);
+	}
+}
+
+void	ft_print_tens(char c, char d, char **keys, char **values)
+{
+	char	str[3];
+	int		index;
+
+	str[0] = c;
+	str[1] = d;
+	str[2] = '\0';
+	index = ft_find_index(str, keys);
+	ft_putstr(values[index]);
+}
+
+void	ft_print_dozens(char c, char **keys, char **values)
+{
+	int		index;
+	char	str[3];
+
+	str[0] = c;
+	str[1] = '0';
+	str[2] = '\0';
+	if (c != 0)
+	{
+		index = ft_find_index(str, keys);
+		ft_putstr(values[index]);
+	}
+}
+
+void	ft_print_units(char c, char **keys, char **values)
+{
+	char	str[2];
+	int		index;
+
+	str[0] = c;
+	str[1] = '\0';
+	if (c != 0)
+	{
+		index = ft_find_index(str, keys);
+		ft_putstr(values[index]);
+	}
+}
--- header_files/print_digit.h
+++ header_files/print_digit.h
@@ -1,43 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   classics.c                                         :+:      :+:    :+:   */
+/*   print_digit.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbenbell <sbenbell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/25 18:05:52 by sbenbell          #+#    #+#             */
-/*   Updated: 2024/02/25 19:21:06 by sbenbell         ###   ########.fr       */
+/*   Created: 2024/02/25 19:04:03 by sbenbell          #+#    #+#             */
+/*   Updated: 2024/02/25 19:09:29 by sbenbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putstr(char *str)
-{
-	int	i;
+#ifndef PRINT_DIGIT_H
+# define PRINT_DIGIT_H
 
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-}
+void	ft_print_zero(char c, char **keys, char **values);
+void	ft_print_hundreds(char c, char **keys, char **values);
+void	ft_print_tens(char c, char d, char **keys, char **values);
+void	ft_print_dozens(char cm char **keys, char **values);
+void	ft_print_units(char c, char **keys, char **values);
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && s1[i] == s2[i])
-		i++;
-	return (s1[i] - s2[i]);
-}
+#endif
